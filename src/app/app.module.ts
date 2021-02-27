6122a7043d37180c170c226809f44342f851d425
sasneprasanna <psasne@oakland.edu>
2021-02-27 16:45:32 -0500

    Buy-Inventory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RatingModule } from "primeng/rating";
+import { PaginatorModule } from "primeng/paginator";
+import { HttpClientModule } from "@angular/common/http";
+import { Routes, RouterModule } from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { LoginComponent } from './login/login.component';
 import { BuyListComponent } from './directives/buy-list/buy-list.component';
+import { JunkBuyComponent } from './components/junkyard-owner/junk-buy/junk-buy.component';
+import { TestComponent } from './test/test.component';
+import { HttpErrorHandler } from './services/http-error-handler.service';
+import { MessageService } from './services/message.service';
+
+const appRoutes: Routes = [
+  { path: 'sell-inventory', component: TestComponent },
+  { path: 'sell-history', component: TestComponent},
+  { path: '', component: BuyListComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    BuyListComponent
+    BuyListComponent,
+    JunkBuyComponent,
+    TestComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    RatingModule,
+    PaginatorModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [MessageService, HttpErrorHandler],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/directives/buy-list/buy-list.component.ts
+++ src/app/directives/buy-list/buy-list.component.ts
@@ -1,15 +1,134 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup} from '@angular/forms';
+import { BuyItems } from '../../shared/models/buyItems.model';
+import { BuyService } from '../../services/buy.service';
 
 @Component({
   selector: 'app-buy-list',
   templateUrl: './buy-list.component.html',
-  styleUrls: ['./buy-list.component.css']
+  styleUrls: ['./buy-list.component.css'],
+  providers: [BuyService]
 })
-export class BuyListComponent implements OnInit {
-  value: number = 40;
-  constructor() { }
 
-  ngOnInit(): void {
+export class BuyListComponent implements OnInit {
+  makers: {"makeId": number, "make": string}[] = [];  
+  years: {"yearId": number, "year": number}[] = [];
+  models: {"modelId": number, "model": string}[] = [];
+  parts: {"partId": number, "part": string}[] = [];
+  states: {"stateId": number, "state": string}[] = [];
+  buyItems: BuyItems[] = [];
+
+  //Initial values for select
+  selectedMake: {"makeId": number, "make": string} = {"makeId": 0, "make": ""};
+  selectedModel: {"modelId": number, "model": string} = {"modelId": 0, "model": ""};
+  selectedstate: {"stateId": string, "state": string} = {"stateId": "*", "state": ""};
+  selectedYear: {"yearId": number, "year": string} = {"yearId": 0, "year": "*"};
+  selectedPart: {"partId": number, "part": string} = {"partId": 0, "part": ""};
+
+  //Flag for validation of select control
+  modelFlag:boolean = true;
+  yearStateFlag: boolean = true;
+  rating: number = 2;
+  nextRecordFlag: boolean = true;
+  previousRecordFlag: boolean = true;
+  startIndex = 0;
+  currentPageIndex = 1;
+
+  constructor(private buyService: BuyService) { 
+    this.generateYears();
+    this.resetFilters();
   }
+
+  // Generate list of past 50 years from current year 
+  generateYears():void {
+    var currentYear = new Date().getFullYear();
+    let startYear = (currentYear-50) || 1980;  
+    let index = 0;
+    while ( currentYear >= startYear ) {
+      this.years.push({"year": currentYear--, "yearId": index++});
+    }
+  }
+
+  /* Clear all dropdowns and disable these fileds on clearing make */ 
+  onChangeMake(event: any) {
+    if(event.value == null){
+      this.selectedModel = {"modelId": 0, "model": ""};
+      this.selectedstate = {"stateId": "*", "state": ""};
+      this.selectedYear = {"yearId": 0, "year": "*"};
+      this.modelFlag = true;
+      this.yearStateFlag = true; 
+      this.resetFilters();
+    } else {
+      this.buyService.getModels(this.selectedMake.makeId)
+      .subscribe(data => this.models = data);
+      this.modelFlag = false;
+    }
+
+  }
+
+  /* To clear state and year dropdown data and disable these fileds on clearing model */ 
+  onChangeModel(event: any) {
+    if(event.value == null){
+      this.yearStateFlag = true;
+      this.selectedstate = {"stateId": "*", "state": ""};
+      this.selectedYear = {"yearId": 0, "year": "*"};
+    } else {
+      this.yearStateFlag = false;
+    } 
+  }
+
+  // To get previous page data
+  previousPage(): void {
+    this.currentPageIndex--;
+    this.startIndex -= 8; 
+    this.search();
+  }
+
+  // To get next page data
+  nextPage(): void {
+    this.currentPageIndex++;
+    this.startIndex += 8;
+    this.search();
+  }
+
+  // Filter query for invetory buy
+  search() : void {
+    let filterQuery = {
+      makeId : this.selectedMake.makeId,
+      modelId: this.selectedModel.modelId,
+      year: this.selectedYear == null? '*': this.selectedYear.year,
+      stateId: this.selectedstate == null ? '*': this.selectedstate.stateId,
+      startIdx: this.startIndex,
+      resultSize: 9
+    }
+    this.buyService.getBuyVehicles(filterQuery).subscribe(data => {
+      //deep-copy of data array to buyItems
+      this.buyItems = JSON.parse(JSON.stringify(data));
+      //trim last record
+      this.buyItems.splice(8, 1);
+
+      // console.log(this.buyItems);
+      if(data.length % 9 == 0){
+        this.nextRecordFlag = false;
+      } else this.nextRecordFlag = true;  
+    });
+    this.currentPageIndex > 1 ? this.previousRecordFlag = false: this.previousRecordFlag = true; 
+    
+  }
+
+  // To reset all filterdata and load data without filter
+  resetFilters(): void {
+    this.selectedMake = {"makeId": 0, "make": ""};
+    this.selectedModel = {"modelId": 0, "model": ""};
+    this.selectedstate = {"stateId": "*", "state": ""};
+    this.selectedYear = {"yearId": 0, "year": "*"};
+    this.modelFlag = true;
+    this.buyService.getMakers().subscribe(data => this.makers = data);
+    this.buyService.getStates().subscribe(data => this.states = data);
+    this.buyService.getParts().subscribe(data => this.parts = data);
+    this.buyService.getBuyVehicles({}).subscribe(data => this.buyItems= data);
+  }
+
+  ngOnInit(): void { }
 
 }
